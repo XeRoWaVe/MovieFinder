0b4b5eba3d1b7d26fb3dd7ad771edb202a71fb3a
Aaron Craig <xerowave22@hotmail.com>
2024-04-22 16:21:03 -0500

    Added movie and show list features. Recolored the header, movies and shows button. Added a status for which list is selected.

--- src/util.ts
+++
@@ -1,8 +0,0 @@
-export const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMmNhYWMxM2QzM2FiNGY2NTczZWZhZjc3MmMyNmJjMyIsInN1YiI6IjY1MDA2NjUxMWJmMjY2MDExYzc4MjU4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mfZLaDd8VJTN9sSVGADmpLH44Ph3Wy-n7U3eAI1y108",
-    },
-  };
